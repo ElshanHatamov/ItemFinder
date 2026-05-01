0c66b6671512779eba933f0bed3aa12bc7d37e3a
Elsen Hetemov <elsen.hetemov123@gmail.com>
2026-05-02 01:41:35 +0400

    Fix: database config with environment variables

--- src/main/java/com/example/itemfinderapplication/model/dto/response/LoginResponse.java
+++ src/main/java/com/example/itemfinderapplication/model/dto/response/LoginResponse.java
@@ -1,10 +1,8 @@
 package com.example.itemfinderapplication.model.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+
 public record LoginResponse(
         @JsonProperty("access_token") String accessToken,
         @JsonProperty("refresh_token") String refreshToken) {
